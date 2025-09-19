3bb9fd01bdf8501e914490192d0326c19df1007e
Joseph-VTs <compcopycode@gmail.com>
2025-09-19 10:35:46 -0300

    Criando e Mudando

--- src/Funcionarios.java
+++ src/Funcionarios.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Funcionarios {
+    // Atributos
+    private String Nome;
+    private int Idade;
+
+    // Métodos Construtores
+    public Funcionarios(String Nome, int Idade){
+        this.Nome = Nome;
+        this.Idade = Idade;
+    }
+
+    // Métodos
+    public void setNome(String nome_Alter) {
+        Nome = nome_Alter;
+    }
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setIdade(int idade_Alter) {
+        Idade = idade_Alter;
+    }
+    public int getIdade() {
+        return Idade;
+    }
+
+    public void Fazer_Aniversario(){
+        Idade++;
+    }
 }
