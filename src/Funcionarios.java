e81c0fd74c6b7bcb32f35ae0262d8b473427c8eb
Joseph-VTs <compcopycode@gmail.com>
2025-09-19 11:56:58 -0300

    Testes com Private

--- src/Funcionarios.java
+++ src/Funcionarios.java
@@ -27,4 +27,35 @@ public class Funcionarios {
     public void Fazer_Aniversario(){
         Idade++;
     }
+
+    @Override
+    public String toString() {
+        return "Funcionário {" + this.Nome + "}, têm {" +  this.Idade + "} anos de Idade";
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        // Criação de Instância 'Objetos'
+        Funcionarios f1 = new Funcionarios("Rosvaldo", 54);
+
+        // Obtendo e Exibindo os valores dos Atributos
+        System.out.println("\n--------------------------");
+        System.out.println("Nome: " + f1.getNome());
+        System.out.println("Idade: " + f1.getIdade());
+
+        System.out.println("\n--------------------------");
+        // Criando outra Instância 'Objeto'
+        Funcionarios f2 = new Funcionarios("Joaquim", 28);
+        System.out.println("Nome: " + f2.getNome());
+        System.out.println("Idade: " + f2.getIdade());
+
+        System.out.println("\n--------------------------");
+        // Funcionário de Aniversário
+        f1.Fazer_Aniversario();
+        System.out.println("Funcionário {" + f1.getNome() + "}, está completando {" + f1.getIdade() + "} Anos de Idade.");
+    }
 }
