1ff066e756e3961307971333fcfe90ca121eee84
Joseph-VTs <compcopycode@gmail.com>
2025-09-19 16:25:34 -0300

    Nova Classe Aluno

--- src/Desafios/Aluno.java
+++ src/Desafios/Aluno.java
@@ -1,18 +1,23 @@
-package Desafios;/*
-    Nome do Aluno
-    Endereço Residencial
-    Telefone de Contato
-    Endereço de E-mail
-    Matrícula
-*/
+package Desafios;
 
-public class Matricula {
+public class Aluno {
+    // Atributos
+    String Nome_Aluno;
+    String Endereco_Residencial;
+    String Telefone;
+    String Email;
+    int Matricula;
 
-    class Aluno{
+    public static void main(String[] args) {
+        Aluno a2 = new Aluno();
 
-    }
-    
-    class Principal{
+        a2.Nome_Aluno = "Joertonino";
+        a2.Endereco_Residencial = "Semangaloirei";
+        a2.Telefone = "51 9.1234-1234";
+        a2.Email = "Aluno_2@gmail.com";
+        a2.Matricula = 370824;
 
+        System.out.println("\nNome: " + a2.Nome_Aluno + " Matrícula: " + a2.Matricula + " Endereço: "
+                + a2.Endereco_Residencial + " Telefone: " + a2.Telefone + " E-mail: " + a2.Email);
     }
 }
